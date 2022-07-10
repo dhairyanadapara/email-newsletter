b67c3720c8acd8b9874e70dcb2a0fcd8ab79ff80
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2022-07-10 23:39:50 +0530

    build fixes

--- src/configuration.rs
+++ src/configuration.rs
@@ -60,9 +60,10 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     settings.merge(
         config::File::from(configuration_directory.join(environment.as_str())).required(true),
     )?;
-    settings.try_into()
 
     settings.merge(config::Environment::with_prefix("app").separator("__"))?;
+
+    settings.try_into()
 }
 
 pub enum Environment {
