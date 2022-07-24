42cd923a0d79edc975e26aade7950e8b6405128f
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2022-07-24 21:51:18 +0530

    email validation changes

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -22,7 +22,9 @@ impl AsRef<str> for SubscriberEmail {
 #[cfg(test)]
 mod tests {
     use super::SubscriberEmail;
-    use claim::{assert_err, assert_ok};
+    use claim::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
 
     #[test]
     fn email_string_is_rejected() {
@@ -48,9 +50,18 @@ mod tests {
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[test]
-    fn email_string_valid_address() {
-        let email = "johndoe@example.com".to_string();
-        assert_ok!(SubscriberEmail::parse(email));
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn email_string_valid_address(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,7 +10,17 @@ pub struct FormData {
     name: String,
 }
 
-/** Page 68 **/
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(name)?;
+        let email = SubscriberEmail::parse(email)?;
+        Ok(NewSubscriber {name, email})
+    }
+}
+
 #[tracing::instrument(
     name="Adding new subscriber",
     skip(form, pool),
@@ -21,17 +31,11 @@ pub struct FormData {
 )]
 #[post("/subscriptions")]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
 
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let new_subscriber = NewSubscriber { email, name };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
