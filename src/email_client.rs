d838cd827c8c5939fd4155eb8e513d60fdcdc76a
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2022-08-05 00:35:18 +0530

    feat: send email

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,4 +1,5 @@
 use crate::domain::SubscriberEmail;
+use reqwest::header;
 use reqwest::Client;
 use secrecy::ExposeSecret;
 use secrecy::Secret;
@@ -14,7 +15,7 @@ pub struct EmailClient {
 impl EmailClient {
     pub fn new(base_url: String, sender: SubscriberEmail, auth_token: Secret<String>) -> Self {
         Self {
-            http_client: Client::new(),
+            http_client: Client::builder().build().unwrap(),
             base_url,
             sender,
             auth_token,
@@ -25,21 +26,20 @@ impl EmailClient {
         &self,
         recipient: SubscriberEmail,
         subject: &str,
-        html_content: &str,
-        text_content: &str,
+        text: &str,
     ) -> Result<(), reqwest::Error> {
         let url = format!("{}/message", self.base_url);
 
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipient.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            text: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipient.as_ref(),
+            subject: subject,
+            text: text,
         };
         let builder = self
             .http_client
-            .basic_auth("api", self.auth_token.expose_secret())
             .post(&url)
+            .header(header::AUTHORIZATION, self.auth_token.expose_secret())
             .json(&request_body)
             .send()
             .await?;
@@ -48,11 +48,12 @@ impl EmailClient {
 }
 
 #[derive(serde::Serialize)]
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    text: String,
+#[serde(rename_all = "PascalCase")]
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    text: &'a str,
 }
 
 #[cfg(test)]
@@ -60,20 +61,42 @@ mod tests {
 
     use crate::domain::SubscriberEmail;
     use crate::email_client::EmailClient;
+    use claim::assert_ok;
     use fake::faker::internet::en::SafeEmail;
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
-    use wiremock::matchers::any;
-    use wiremock::{Mock, MockServer, ResponseTemplate};
+    use reqwest::header;
     use secrecy::Secret;
+    use wiremock::matchers::*;
+    use wiremock::{Mock, MockServer, Request, ResponseTemplate};
+
+    struct SendEmailBodyMatcher;
+
+    impl wiremock::Match for SendEmailBodyMatcher {
+        fn matches(&self, request: &Request) -> bool {
+            let result: Result<serde_json::Value, _> = serde_json::from_slice(&request.body);
+            if let Ok(body) = result {
+                body.get("From").is_some()
+                    && body.get("To").is_some()
+                    && body.get("Subject").is_some()
+                    && body.get("Text").is_some()
+            } else {
+                false
+            }
+        }
+    }
 
     #[tokio::test]
-    async fn send_email_request_to_base_url() {
+    async fn send_email_succeeds_if_the_server_returns_200() {
         let mock_server = MockServer::start().await;
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
 
-        Mock::given(any())
+        Mock::given(header_exists(header::AUTHORIZATION))
+            .and(header("Content-Type", "application/json"))
+            .and(path("/message"))
+            .and(method("POST"))
+            .and(SendEmailBodyMatcher)
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
@@ -83,8 +106,10 @@ mod tests {
         let subject: String = Sentence(1..2).fake();
         let content: String = Paragraph(1..10).fake();
 
-        let _ = email_client
-            .send_email(subscriber_email, &subject, &content, &content)
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content)
             .await;
+
+        assert_ok!(outcome);
     }
 }
