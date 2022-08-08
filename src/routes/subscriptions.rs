73d14763a5eef2584d79c11eb863d56a70bcc89e
Dhairya Nadapara <dhairya@zuru.tech>
2022-08-08 19:43:10 +0530

    added db migration to add new column and table

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -48,8 +48,8 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
 pub async fn insert_subscriber(pool: &PgPool, form: &NewSubscriber) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
+        INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+        VALUES ($1, $2, $3, $4, 'confirmed')
         "#,
         Uuid::new_v4(),
         form.email.as_ref(),
